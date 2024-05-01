48f8eb9269bd4f4ad67a18be7448355261cd0de3
danjeljansson <Daniel.Jansson@yh.nackademin.se>
2024-05-01 21:24:29 +0200

    database model

---
+++ server/src/model/model.ts
@@ -0,0 +1,34 @@
+import { DataTypes, Model } from "sequelize";
+import db from "../config/database.config";
+
+interface QuizAttributes {
+  id: number;
+  question: string;
+  answer: string;
+}
+
+class QuizInstance extends Model<QuizAttributes> {}
+
+QuizInstance.init(
+  {
+    id: {
+      type: DataTypes.UUIDV4,
+      primaryKey: true,
+      autoIncrement: false,
+    },
+    question: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    answer: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: "Quiz",
+  },
+);
+
+export default QuizInstance;
