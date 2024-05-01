88912137a7f8a2ae5030df7bee4ffe3386ba9793
danjeljansson <Daniel.Jansson@yh.nackademin.se>
2024-05-01 21:35:54 +0200

    ts files

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,17 +1,45 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-// configures dotenv to work in your application
+const database_config_1 = __importDefault(require("./config/database.config"));
+const model_1 = __importDefault(require("./model/model"));
+const uuid_1 = require("uuid");
+const quiz_validator_1 = __importDefault(require("./validator/quiz-validator"));
+const validate_dto_1 = __importDefault(require("./middleware/validate-dto"));
 dotenv_1.default.config();
-const app = (0, express_1.default)();
-const PORT = process.env.PORT;
-app.get("/", (request, response) => {
-    response.status(200).send("Hello World");
+database_config_1.default.sync().then(() => {
+    console.log("Database connected");
 });
+const PORT = process.env.PORT;
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.get("/", (request, response) => {
+    console.log(request.body);
+    return response.send("Hello World");
+});
+app.post("/create", (0, validate_dto_1.default)(quiz_validator_1.default), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = (0, uuid_1.v4)();
+    try {
+        const question = yield model_1.default.create(Object.assign(Object.assign({}, req.body), { id }));
+        res.json({ question, msg: "Success" });
+    }
+    catch (e) {
+        res.status(500).json({ msg: "failed", route: "/create" });
+    }
+}));
 app
     .listen(PORT, () => {
     console.log("Server running at PORT: ", PORT);
--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,23 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
+var express_1 = require("express");
+var dotenv_1 = require("dotenv");
+var database_config_1 = require("./config/database.config");
 dotenv_1.default.config();
-const app = (0, express_1.default)();
-const port = process.env.PORT;
-app.get("/", (req, res) => {
-    res.send("Express + TypeScript Server");
+database_config_1.default.sync().then(function () {
+    console.log("Database connected");
 });
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+var PORT = process.env.PORT;
+var app = (0, express_1.default)();
+app.get("/", function (response) {
+    return response.send("Hello World");
 });
+app
+    .listen(PORT, function () {
+    console.log("Server running at PORT: ", PORT);
+})
+    .on("error", function (error) {
+    // gracefully handle error
+    throw new Error(error.message);
+});
+// Path: server/src/index.ts
