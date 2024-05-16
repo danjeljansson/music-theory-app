aba0485927a9e1eb5a3430436fb8993ff26eb297
danjeljansson <Daniel.Jansson@yh.nackademin.se>
2024-05-16 16:17:53 +0200

    added error message

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,20 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import router from "./routes/index";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/", router);
+
+app.use((error: Error, req: Request, res: Response) => {
+  console.error(error.stack);
+  res.status(500).send("Something broke!");
+});
 
 export default app;
